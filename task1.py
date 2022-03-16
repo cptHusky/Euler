8e38e78799bd3a9e9142b9e1465ac45168a5afd9
cptHusky <101748691+cptHusky@users.noreply.github.com>
2022-03-16 22:58:31 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,35 @@
+# answer is 233168 for LIMIT = 1000
+import time
+
+LIMIT = 1000
+
+
+def arif_seq_sum(step):
+    local_limit = LIMIT - 1
+    first_number = step
+    last_number = int(local_limit/step) * step
+    sum = ((first_number + last_number) * int(local_limit/step))/2
+    return sum
+
+
+if __name__ == '__main__':
+    t = time.time()
+    sum3 = arif_seq_sum(3)
+    sum5 = arif_seq_sum(5)
+    sum15 = arif_seq_sum(15)
+    print(sum3, sum5, sum15)
+    print(sum3 + sum5 - sum15)
+    print(time.time() - t, '\n\n')
+
+
+# --------------STUPID WAY--------------------
+if __name__ == '__main__':
+    t = time.time()
+    sum = 0
+    for number in range(1, LIMIT):
+        if number % 3 == 0:
+            sum += number
+        elif number % 5 == 0:
+            sum += number
+    print(sum)
+    print(time.time() - t, '\n\n')
