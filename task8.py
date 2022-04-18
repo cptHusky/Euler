7190614d8aee2138c12826d83c2ffc619deeaa32
Petr Alekseev <petr.alekseev@ntprog.com>
2022-04-18 08:46:16 +0300

    task 8 done

--- task8.py
+++ task8.py
@@ -1,21 +1,23 @@
 import re
 from collections import deque
 
-FILENAME = '1000DigitNumberS.txt'
+FILENAME = '1000DigitNumber.txt'
 AMOUNT_IN_SEQ = 13
+products = {}
 
 
 def find_max_seq(big_set, seq_len):
-    print(f'big set {big_set}\npredicted {len(big_set) - seq_len + 1} iterations\n')
+    global products
+    # print(f'big set {big_set}\npredicted {len(big_set) - seq_len + 1} iterations\n')
     for i in range(len(big_set) - seq_len + 1):
         small_set = big_set[i:seq_len + i]
-        print(f'iteration {i} with\nset {small_set}')
+        # print(f'iteration {i} with\n{small_set}')
         numbers = deque(small_set)
         product = 1
         for m in range(seq_len):
             product *= int(numbers.pop())
-        print(f'product {product}')
-        products = {small_set: product}
+        # print(f'product is {product}')
+        products[small_set] = product
 
 
 if __name__ == '__main__':
@@ -23,15 +25,8 @@ if __name__ == '__main__':
         digits = file.read()
         sets = re.split('0', digits)
     file.close()
-
-    sets[1] = '12345678123456'
-    for i in range(len(sets)):
-        if len(sets[i]) >= AMOUNT_IN_SEQ:
-            print(f'set number {i} length {len(sets[i])}|{sets[i]}')
-    print()
-
     for i in range(len(sets)-1):
-        print(f'\nset number {i} length {len(sets[i])}')
+        # print(f'\nset number {i} length {len(sets[i])}')
         if len(sets[i]) >= AMOUNT_IN_SEQ:
             find_max_seq(sets[i], AMOUNT_IN_SEQ)
-    # print('\n\n', products)
+    print(max(products.items(), key=lambda k: k[1]))
